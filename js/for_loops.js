a2ea69f1e0a1fd9e8dc5d9856834ca105f99d415
Christopher Gamez <ChrisxGamez@gmail.com>
2020-06-29 16:23:51 -0500

    Updated

---
+++ js/for_loops.js
@@ -0,0 +1,30 @@
+console.log("Hi");
+
+
+// 2.
+// for (let i = 1, j = 7; i <= 10; i++) {
+//     console.log('7 x ' + i + ' = ' + (j * i));
+// }
+
+
+// 3.
+// for (let i = 0; i < 10; i++) {
+//     var x = Math.floor(Math.random() * 180) + 20;
+//     if (x % 2 === 0) {
+//         console.log(x + " is even")
+//     } else {
+//         console.log(x + " is odd")
+//     }
+// }
+
+
+// 4.
+// for(i = 1; i <= 9; i++) {
+//     console.log(String(i).repeat(i))
+// }
+
+
+// // 5.
+//     for (let i = 105; i > 0; i--)
+//         if (i % 5 === 0)
+//             console.log(i);
