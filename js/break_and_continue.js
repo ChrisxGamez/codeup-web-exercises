99807293eb1d3d0c5a7a8b5e145ea4bb91506811
Christopher Gamez <ChrisxGamez@gmail.com>
2020-06-30 11:06:28 -0500

    Complete

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,22 +1,30 @@
-// alert(" Let's play a guessing game!")
-// let guess = prompt("Pick an odd number");
-// while (guess % 2 !== 0) {
-//     console.log("Skip " + guess)
-// } else {
-//     alert("Try again");
-// }
+alert("Let's play a guessing game");
+let guess;
+do {
+    guess = prompt("I chose an odd number between 1 and 50. Can you guess what it is?");
+    if (guess % 2 === 0) {
+        alert("No, that was an even number. Try again");
+        continue;
+    } else {
+        console.log("Number to skip: " + guess);
+        break;
+    }
+} while (true);
+
+    for (let i = 1; i <= 50; i += 1) {
+
+        if (i % 2 === 0) {
+            continue;
+
+        }   if (i != guess) {
+            console.log("Here is an odd number: " + i)
+
+        } else {
+            console.log("Yikes! Skipping number " + guess)
+        }
+    }
 
 
 
-// }
-//     for (var i = 1; i <= 50; i++) {
-//         if (i !== guess) {
-//             console.log(i);
-//         } else if (i == guess) {
-//             console.log("Skip")
-//             break;
-//
-//         }
-//
-//     }
+
 
