69face88a646657ab4e076d1e85feb470718b62f
Christopher Gamez <ChrisxGamez@gmail.com>
2020-06-29 19:13:18 -0500

    Updated

---
+++ js/break_and_continue.js
@@ -0,0 +1,22 @@
+// alert(" Let's play a guessing game!")
+// let guess = prompt("Pick an odd number");
+// while (guess % 2 !== 0) {
+//     console.log("Skip " + guess)
+// } else {
+//     alert("Try again");
+// }
+
+
+
+// }
+//     for (var i = 1; i <= 50; i++) {
+//         if (i !== guess) {
+//             console.log(i);
+//         } else if (i == guess) {
+//             console.log("Skip")
+//             break;
+//
+//         }
+//
+//     }
+
